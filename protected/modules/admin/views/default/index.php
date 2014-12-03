e678d8aabd74e73560a4ec0581cef781d7ed4e37
Prakhash <prakhashsiva21@gmail.com>
2014-12-03 21:41:44 +0530

    Admin dashboard Modified

--- protected/modules/admin/views/default/index.php
+++ protected/modules/admin/views/default/index.php
@@ -8,43 +8,43 @@ $this->breadcrumbs = array(
     <div class="span12">
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Course Management</span>', array('/admin/course'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Crew</span>', array('/admin/course'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
 
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Level Management</span>', array('/admin/level'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Blogs</span>', array('/admin/level'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
 
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Subject Management</span>', array('/admin/subject'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Users</span>', array('/admin/subject'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
 
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Subject Area Management</span>', array('/admin/subjectArea'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Comments</span>', array('/admin/subjectArea'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
 
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Sitting Management</span>', array('/admin/sitting'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Album</span>', array('/admin/sitting'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
 
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">News Management</span>', array('/admin/news'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Events</span>', array('/admin/news'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
 
         <div class="span3">
             <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Country Management</span>', array('/admin/country'), array('class' => 'shortcutadmin')); ?>
+                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Manage Sub-events</span>', array('/admin/country'), array('class' => 'shortcutadmin')); ?>
             </div> <!-- /shortcuts -->	
         </div>
     </div>
@@ -53,42 +53,5 @@ $this->breadcrumbs = array(
         <hr class="hrstyle">
     </div>
 
-    <div class="span12">
-        <div class="span3">
-            <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Student Management</span>', array('/admin/student'), array('class' => 'shortcutadmin')); ?>
-            </div> <!-- /shortcuts -->	
-        </div>
-
-        <div class="span3">
-            <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Lecture Management</span>', array('/admin/lecturer'), array('class' => 'shortcutadmin')); ?>
-            </div> <!-- /shortcuts -->	
-        </div>
-
-        <div class="span3">
-            <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Temporary Users</span>', array('/admin/temporaryUser'), array('class' => 'shortcutadmin')); ?>
-            </div> <!-- /shortcuts -->	
-        </div>
-
-        <div class="span3">
-            <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Exam Management</span>', array('/admin/exam'), array('class' => 'shortcutadmin')); ?>
-            </div> <!-- /shortcuts -->	
-        </div>
-
-        <div class="span3">
-            <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Question Management</span>', array('/admin/question'), array('class' => 'shortcutadmin')); ?>
-            </div> <!-- /shortcuts -->	
-        </div>
-
-        <div class="span3">
-            <div class="shortcutsadmin">
-                <?php echo CHtml::link('<i class="icon-zoom-in text-large"></i><span class="shortcut-label">Result Management</span>', array('/admin/result'), array('class' => 'shortcutadmin')); ?>
-            </div> <!-- /shortcuts -->	
-        </div>
-    </div>
-
+   
 </div>  
