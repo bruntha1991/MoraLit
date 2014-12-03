5d40695b453ec1ea56911db82389990986fbd87f
Prakhash <prakhashsiva21@gmail.com>
2014-12-03 20:39:49 +0530

    Admin dashboard added

--- protected/modules/admin/views/layouts/column1.php
+++ protected/modules/admin/views/layouts/column1.php
@@ -1,4 +1,5 @@
-<?php $this->beginContent('/layouts/main'); ?>
+<?php /* @var $this Controller */ ?>
+<?php $this->beginContent('//layouts/main'); ?>
 <div id="content">
 	<?php echo $content; ?>
 </div><!-- content -->
