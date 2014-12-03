7e92c0f55e40edd0d702c5245f18f420a100f7df
Prakhash <prakhashsiva21@gmail.com>
2014-12-03 20:50:33 +0530

    Admin dashboard added

--- protected/modules/admin/views/layouts/column2.php
+++ protected/modules/admin/views/layouts/column2.php
@@ -1,30 +1,21 @@
-
-<?php $this->beginContent('/layouts/main'); ?>
-
-<div class="span9">    
-        <div class="span-5 last operations_ads">
-            <div id="sidebar">
-
-                    <?php   
-                            $this->beginWidget('zii.widgets.CPortlet', array(
-                                    //'title'=>'Operations',
-                            ));
-                            $this->widget('bootstrap.widgets.TbMenu', array(
-                            'items'=>$this->menu,
-                                            'htmlOptions'=>array('class'=>'operations'),
-                            ));
-                            $this->endWidget();
-                    ?>
-             </div>
-          </div>	
-</div>
-
-    <div class="span9">
-<!--        <div id="content">-->
+<?php $this->beginContent('//layouts/main'); ?>
+<div class="span-19">
+	<div id="content">
 		<?php echo $content; ?>
-	<!--</div> content -->
+	</div><!-- content -->
 </div>
-
-
-    <!--</div>-->
+<div class="span-5 last">
+	<div id="sidebar">
+	<?php
+		$this->beginWidget('zii.widgets.CPortlet', array(
+			'title'=>'Operations',
+		));
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->menu,
+			'htmlOptions'=>array('class'=>'operations'),
+		));
+		$this->endWidget();
+	?>
+	</div><!-- sidebar -->
+</div>
 <?php $this->endContent(); ?>
