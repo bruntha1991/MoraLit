1d0edd0094191310619a64fae9cdb6fd4229ebc9
AARTHIKA <AARTHIKA@AARTHIKA-TOSH>
2014-12-08 23:14:32 +0530

    Image displayed

--- protected/views/crew/_view.php
+++ protected/views/crew/_view.php
@@ -5,43 +5,72 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('crew_id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->crew_id), array('view', 'id'=>$data->crew_id)); ?>
-	<br />
+    <!-- Start Profile -->
+    <div class="span4 profile">
+        <div class="image-wrap">
+            <div class="hover-wrap">
+                <span class="overlay-img"></span>
+                <span class="overlay-text-thumb">
+<!--                    <b><?php echo CHtml::encode($data->getAttributeLabel('designation')); ?>:</b>-->
+                    <h3 class="profile-name">
+                        <!--<b><?php echo CHtml::encode($data->designation); ?></b>-->
+                        <?php echo CHtml::link(CHtml::encode($data->designation), array('view', 'id' => $data->crew_id)); ?>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('first_name')); ?>:</b>
-	<?php echo CHtml::encode($data->first_name); ?>
-	<br />
+                        <br />
+                    </h3>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('last_name')); ?>:</b>
-	<?php echo CHtml::encode($data->last_name); ?>
-	<br />
+                </span>
+            </div>
+            <img src="<?php echo CHtml::encode($data->image);?>" alt="" class="img-circle">
+        </div>
+        <h5 class="profile-name">
+            <?php echo CHtml::encode($data->first_name); ?>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('faculty')); ?>:</b>
-	<?php echo CHtml::encode($data->faculty); ?>
-	<br />
+            <?php echo CHtml::encode($data->last_name); ?>
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('year')); ?>:</b>
-	<?php echo CHtml::encode($data->year); ?>
-	<br />
+        </h5>
+        <p class="profile-description">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('designation')); ?>:</b>
-	<?php echo CHtml::encode($data->designation); ?>
-	<br />
+    </div>
+    <!-- End Profile -->
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_no')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_no); ?>
-	<br />
+<!--	<b><?php echo CHtml::encode($data->getAttributeLabel('crew_id')); ?>:</b>
+    <?php echo CHtml::link(CHtml::encode($data->crew_id), array('view', 'id' => $data->crew_id)); ?>
+        <br />
 
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
-	<?php echo CHtml::encode($data->email); ?>
-	<br />
+        <b><?php echo CHtml::encode($data->getAttributeLabel('first_name')); ?>:</b>
+    <?php echo CHtml::encode($data->first_name); ?>
+        <br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('image')); ?>:</b>
-	<?php echo CHtml::encode($data->image); ?>
-	<br />
+        <b><?php echo CHtml::encode($data->getAttributeLabel('last_name')); ?>:</b>
+    <?php echo CHtml::encode($data->last_name); ?>
+        <br />
 
-	*/ ?>
+        <b><?php echo CHtml::encode($data->getAttributeLabel('faculty')); ?>:</b>
+    <?php echo CHtml::encode($data->faculty); ?>
+        <br />
+
+        <b><?php echo CHtml::encode($data->getAttributeLabel('year')); ?>:</b>
+    <?php echo CHtml::encode($data->year); ?>
+        <br />
+
+        <b><?php echo CHtml::encode($data->getAttributeLabel('designation')); ?>:</b>
+    <?php echo CHtml::encode($data->designation); ?>
+        <br />
+
+        <b><?php echo CHtml::encode($data->getAttributeLabel('contact_no')); ?>:</b>
+    <?php echo CHtml::encode($data->contact_no); ?>
+        <br />-->
+
+    <?php /*
+      <b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+      <?php echo CHtml::encode($data->email); ?>
+      <br />
+
+      <b><?php echo CHtml::encode($data->getAttributeLabel('image')); ?>:</b>
+      <?php echo CHtml::encode($data->image); ?>
+      <br />
+
+     */ ?>
 
 </div>
