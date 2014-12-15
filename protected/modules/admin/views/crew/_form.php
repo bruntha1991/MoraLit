b695368e3fbbb1572519f0a3f901b80226683e8f
aarthika-nathan <aarthika.nathan15@gmail.com>
2014-12-15 22:04:20 +0530

    Merge branch 'master' of git://github.com/Prakhash/Moralit
    Conflicts:
    	protected/runtime/application.log
    	protected/views/crew/index.php

--- protected/modules/admin/views/crew/_form.php
+++ protected/modules/admin/views/crew/_form.php
@@ -17,88 +17,87 @@
 )); ?>
 
 
+	
+<div class="form">
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($model); ?>
-
+        
+        <table class="table table-striped table-bordered" style="width:80%;">
+            <col width="240">
+            <col width="130">
 	<div class="row">
-		<?php echo $form->labelEx($model,'first_name'); ?>
-		<?php echo $form->textField($model,'first_name',array('size'=>45,'maxlength'=>45)); ?>
+            <tr>
+            <td><?php echo $form->labelEx($model,'first_name'); ?></td>
+            <td><?php echo $form->textField($model,'first_name',array('size'=>45,'maxlength'=>45)); ?></td>
 		<?php echo $form->error($model,'first_name'); ?>
+            </tr>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'last_name'); ?>
-		<?php echo $form->textField($model,'last_name',array('size'=>45,'maxlength'=>45)); ?>
+            <tr>
+            <td><?php echo $form->labelEx($model,'last_name'); ?></td>
+            <td><?php echo $form->textField($model,'last_name',array('size'=>45,'maxlength'=>45)); ?></td>
 		<?php echo $form->error($model,'last_name'); ?>
+            </tr>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'faculty'); ?>
+            <tr>
+            <td><?php echo $form->labelEx($model,'faculty'); ?></td>
            
-                <?php $data = array('','Engineering','Architecture','Information Technology','National Diploma in Technology');
-                echo $form->dropDownList($model,'faculty',array_combine($data, $data)); ?>
+                <td><?php $data = array('','Engineering','Architecture','Information Technology','National Diploma in Technology');
+                echo $form->dropDownList($model,'faculty',array_combine($data, $data)); ?></td>
 		<?php echo $form->error($model,'faculty'); ?>
+            </tr>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'year'); ?>
-		<?php echo $form->textField($model,'year',array('size'=>4,'maxlength'=>4)); ?>
-		<?php echo $form->error($model,'year'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'designation'); ?>
-		<?php $data = array('','President','Vice-President','Secretary',
-                    'Vice-Secretary','Treasurer','Editor','Web-Coordinator','Representative');
-                echo $form->dropDownList($model,'designation',array_combine($data, $data)); ?>
-		<?php echo $form->error($model,'designation'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'contact_no'); ?>
-		<?php echo $form->textField($model,'contact_no',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'contact_no'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->emailField($model,'email'); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
-
-        <div class="row">
-        <?php echo $form->labelEx($model, 'image'); ?>
-        <?php echo CHtml::activeFileField($model, 'image'); ?>  
-        <?php echo $form->error($model, 'image'); ?>
-      
+            <tr>
+            <td><?php echo $form->labelEx($model,'year'); ?></td>
+            <td><?php echo $form->textField($model,'year',array('size'=>4,'maxlength'=>4)); ?></td>
             
-        </div>
-
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+		<?php echo $form->error($model,'year'); ?>
+            </tr>
 	</div>
+
+	<div class="row">
+            <tr>
+            <td><?php echo $form->labelEx($model,'designation'); ?></td>
+		<td><?php $data = array('','President','Vice-President','Secretary',
+                    'Vice-Secretary','Treasurer','Editor','Web-Coordinator','Representative');
+                echo $form->dropDownList($model,'designation',array_combine($data, $data)); ?></td>
+		<?php echo $form->error($model,'designation'); ?>
+            </tr>
+	</div>
+
+	<div class="row">
+            <tr>
+            <td><?php echo $form->labelEx($model,'contact_no'); ?></td>
+            <td><?php echo $form->textField($model,'contact_no',array('size'=>10,'maxlength'=>10)); ?></td>
+		<?php echo $form->error($model,'contact_no'); ?>
+            </tr>
+	</div>
+
+	<div class="row">
+            <tr>
+            <td><?php echo $form->labelEx($model,'email'); ?></td>
+            <td><?php echo $form->emailField($model,'email'); ?></td>
+		<?php echo $form->error($model,'email'); ?>
+            </tr>
+	</div>
+        <div class="row">
+            <tr>
+            <td><?php echo $form->labelEx($model, 'image'); ?></td>
+            <td><?php echo CHtml::activeFileField($model, 'image'); ?>  </td>
+        <?php echo $form->error($model, 'image'); ?>
+            </tr>            
+        </div>
+        <div class="row buttons">
+            <tr>
+                <td colspan="3"><?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?></td>
+            </tr>
+	</div>
+                
+        </table>
 
 <?php $this->endWidget(); ?>
-
-</div>
-</div>    
-    <!-- form -->
-    
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script src="js/vendor/jquery.ui.widget.js"></script>
-<script src="js/jquery.iframe-transport.js"></script>
-<script src="js/jquery.fileupload.js"></script>
-<script>
-$(function () {
-    $('#fileupload').fileupload({
-        dataType: 'json',
-        done: function (e, data) {
-            $.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo(document.body);ok
-            });
-        }
-    });
-});
-</script>
